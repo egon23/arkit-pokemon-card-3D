5e49800d3dd0900ee4783f5c91c0e0dd817bf60d
Roland Iana <roland.iana@students.fh-hagenberg.at>
2020-06-23 18:08:24 +0200

    init

---
+++ Pokemon Card 3D/ViewController.swift
@@ -0,0 +1,119 @@
+//
+//  ViewController.swift
+//  bootcamp
+//
+//  Created by roli on 23.06.20.
+//  Copyright © 2020 Roland Iana. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        sceneView.autoenablesDefaultLighting = true
+        
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARImageTrackingConfiguration()
+        
+        if let imageToTrack = ARReferenceImage.referenceImages(inGroupNamed: "PokeCards", bundle: Bundle.main) {
+            
+            configuration.trackingImages = imageToTrack
+            
+            configuration.maximumNumberOfTrackedImages = 1
+            
+          
+            
+        }
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+*/
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+         
+             let node = SCNNode()
+             
+             if let imageAnchor = anchor as? ARImageAnchor {
+                 
+                 let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+                 
+                 plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
+
+                 let planeNode = SCNNode(geometry: plane)
+                 
+                 planeNode.eulerAngles.x = -.pi / 2
+                 
+                 node.addChildNode(planeNode)
+                 
+                 if imageAnchor.referenceImage.name == "bisasam-card" {
+                     if let pokeScene = SCNScene(named: "art.scnassets/Bulbasaur_ColladaMax copy.scn") {
+                         
+                         if let pokeNode = pokeScene.rootNode.childNodes.first {
+                             
+                             pokeNode.eulerAngles.x = .pi / 2
+                             
+                             planeNode.addChildNode(pokeNode)
+                         }
+                     }
+                 }
+                 
+                 if imageAnchor.referenceImage.name == "schiggy-card" {
+                     if let pokeScene = SCNScene(named: "art.scnassets/shiggy.scn") {
+                         
+                         if let pokeNode = pokeScene.rootNode.childNodes.first {
+                             
+                             pokeNode.eulerAngles.x = .pi / 2
+                             
+                             planeNode.addChildNode(pokeNode)
+                         }
+                     }
+                 }
+                 
+                 
+                 
+                 
+                 
+             }
+             
+             
+             
+             return node
+    }
+ 
+}
